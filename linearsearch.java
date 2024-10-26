f2bcdd7a6cec7082abfd3be12babf56c128d7ef6
NIHKIL JAI <nikhilraina95@gmail.com>
2024-10-26 20:04:12 +0530

    linear search in dsa

---
+++ linearsearch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class linearsearch{
+
+    static boolean search(String str, char ch){
+            if(str.length() == 0){
+                return false;
+            }
+            for(int i = 0; i<str.length() ;i++){
+                if(ch == str.charAt(i)){
+                    return true;
+                }                        
+            }return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = "batman";
+        System.out.println("enter the letter to find");
+        char target = sc.next().charAt(0);
+        System.out.println(search(name ,target));
+    }
+}
